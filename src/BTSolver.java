180ab43d73edbf72ffe3b484f7d4f2319ced350f
dav <dav@pc5-001-l.cs.st-andrews.ac.uk>
2018-04-09 14:14:36 +0100

    backtracking now works

--- src/BTSolver.java
+++ src/BTSolver.java
@@ -1,49 +1,56 @@
 
 public class BTSolver {
-	
-	BinaryCSP problem;
-	int nodesExplored;
 
-	public BTSolver(BinaryCSP problem) {
-		super();
-		this.problem = problem;
-	}
+    BinaryCSP problem;
+    int nodesExplored;
 
-	public static void main(String[] args){
-		
-		String CSPLocation = args[0];
-		BinaryCSPReader reader = new BinaryCSPReader() ;
-		System.out.println(CSPLocation);
-		BTSolver solver = new BTSolver(reader.readBinaryCSP(CSPLocation));
-		solver.solveCurrentProblem();
-	}
-	
-	public void solveCurrentProblem(){
-		backtrack(0);
-//		System.out.println(problem.toString());
-	}
-	
-	// adapted from lecture slides. simple backtracking
-	public boolean backtrack(int depth) { 
-		CSPVariable var = problem.getVar(depth);
-		for(int value = var.getLowerBound(); value < var.getUpperBound(); value++) {
-			var.assign(value);
-			boolean consistent = true;
-			for(BinaryConstraint constr : problem.getConstraints()) {
-				if(!constr.isConsistent()) {
-					continue;
-				}
-			}
-			if(depth == problem.getNoVariables()-1) {
-				System.out.println("Solution: ");
-				System.out.println(problem.toString());
-				return true;
-			} else {
-				return backtrack(depth+1);
-			}
+    public BTSolver(BinaryCSP problem) {
 
-		}
-		return false;
-	}
+        super();
+        this.problem = problem;
+    }
+
+    public static void main(String[] args) {
+
+        String CSPLocation = args[0];
+        BinaryCSPReader reader = new BinaryCSPReader();
+        System.out.println(CSPLocation);
+        BTSolver solver = new BTSolver(reader.readBinaryCSP(CSPLocation));
+        solver.solveCurrentProblem();
+    }
+
+    public void solveCurrentProblem() {
+
+        if (!backtrack(0)) {
+            System.out.println("No solution possible");
+        }
+        //		System.out.println(problem.toString());
+    }
+
+    // adapted from lecture slides. simple backtracking
+    public boolean backtrack(int depth) {
+
+        CSPVariable var = problem.getVar(depth);
+        for (int value = var.getLowerBound(); value <= var.getUpperBound(); value++) {
+            var.assign(value);
+            if (problem.isArcConsistent()) {
+                if (depth == problem.getNoVariables() - 1) {
+                    System.out.println("Solution: ");
+                    System.out.println(problem.toString());
+                    return true;
+                } else {
+                    if (!backtrack(depth + 1)) {
+                        var.unassign();
+                    } else {
+                        return true;
+                    }
+                }
+            } else {
+                var.unassign();
+                continue;
+            }
+        }
+        return false;
+    }
 
 }
--- src/BinaryCSP.java
+++ src/BinaryCSP.java
@@ -1,36 +1,54 @@
-import java.util.* ;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public final class BinaryCSP {
 
-  private ArrayList<CSPVariable> varList;
-  private ArrayList<BinaryConstraint> constraints ;
-  
-  public BinaryCSP(CSPVariable[] domainBounds, ArrayList<BinaryConstraint> c) {
-    varList = new ArrayList<CSPVariable>(Arrays.asList(domainBounds)) ;
-    constraints = c ;
-  }
-  
-  public String toString() {
-    StringBuffer result = new StringBuffer() ;
-    result.append("CSP:\n") ;
-    for (int i = 0; i < varList.size(); i++)
-      result.append(varList.get(i).toString()) ;
-    for (BinaryConstraint bc : constraints)
-      result.append(bc+"\n") ;
-    return result.toString() ;
-  }
-  
-  public int getNoVariables() {
-    return varList.size() ;
-  }
+    private ArrayList<CSPVariable> varList;
+    private ArrayList<BinaryConstraint> constraints;
 
-  
-  public ArrayList<BinaryConstraint> getConstraints() {
-    return constraints ;
-  }
-  
-  public CSPVariable getVar(int i) {
-	  return varList.get(i);
-  }
+    public BinaryCSP(CSPVariable[] domainBounds, ArrayList<BinaryConstraint> c) {
+
+        varList = new ArrayList<CSPVariable>(Arrays.asList(domainBounds));
+        constraints = c;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuffer result = new StringBuffer();
+        result.append("CSP:\n");
+        for (int i = 0; i < varList.size(); i++) {
+            result.append(varList.get(i).toString());
+        }
+        for (BinaryConstraint bc : constraints) {
+            result.append(bc + "\n");
+        }
+        return result.toString();
+    }
+
+    public int getNoVariables() {
+
+        return varList.size();
+    }
+
+    public ArrayList<BinaryConstraint> getConstraints() {
+
+        return constraints;
+    }
+
+    public CSPVariable getVar(int i) {
+
+        return varList.get(i);
+    }
+
+    public boolean isArcConsistent() {
+
+        for (BinaryConstraint constr : getConstraints()) {
+            if (!constr.isConsistent()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
