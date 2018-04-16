e66daa9b3dfe2b8cc4504879e9737abb90e27a4d
dav <dav@pc5-001-l.cs.st-andrews.ac.uk>
2018-04-16 15:41:44 +0100

    add variable and constraint count to data

--- runAllProblems.py
+++ runAllProblems.py
@@ -15,9 +15,9 @@ errorPatern  = re.compile("")
 heuristics = ["Name", "Random", "MaxDegree"]
 call("rm -f " + cspFolder + "/*.csph",shell=True)
 call("java -classpath " + binFolder + " HeuristicGenerator "+cspFolder,shell=True)
-call("rm -f " + logFile + " && touch " + logFile + " && echo \"Algorithm,Heuristic,Problem,Nodes explored,Arcs Revised\" >> "+logFile,shell=True)
+#call("rm -f " + logFile + " && touch " + logFile + " && echo \"Algorithm,Heuristic,Problem,Nodes explored,Arcs Revised\" >> "+logFile,shell=True)
 
-for file in os.listdir(cspFolder):
+for file in sorted(os.listdir(cspFolder)):
     if(problemPatern.match(file)):
         problem = os.path.splitext(file)[0]
         print(problem)
@@ -25,8 +25,9 @@ for file in os.listdir(cspFolder):
         branchSearch = branchPatern.search(BTOutput)
         nodesExplored = branchSearch.group(1)
         arcsRevised = 0
-        lineOut = "Backtracking,None," + problem + ", " + str(nodesExplored) + ", " + str(arcsRevised)
+        lineOut = "Backtracking,None, " + problem + ", " + str(nodesExplored) + ", " + str(arcsRevised)
         call("echo \"" + lineOut + "\" >> "+logFile,shell=True)
+        print(lineOut)
 
         FCSDOutput = check_output("java -classpath " + binFolder + " -ea FCSolver " + cspFolder + file ,shell=True)
         branchSearch = branchPatern.search(FCSDOutput)
@@ -35,8 +36,9 @@ for file in os.listdir(cspFolder):
         arcsRevised = arcSearch.group(1)
         lineOut = "Forward Checking, Smallest Domain, " + problem + ", " + str(nodesExplored) + ", " + str(arcsRevised)
         call("echo \"" + lineOut + "\" >> "+logFile,shell=True)
+        print(lineOut)
 
-        for h in heuristics:
+        for h in  ["Random", "MaxDegree"]:
             FCOutput = check_output("java -classpath " + binFolder + " -ea FCSolver " + cspFolder + file + " " + cspFolder+problem+h+".csph",shell=True)
             branchSearch = branchPatern.search(FCOutput)
             nodesExplored = branchSearch.group(1)
@@ -44,56 +46,23 @@ for file in os.listdir(cspFolder):
             arcsRevised = arcSearch.group(1)
             lineOut = "Forward Checking, " + h + ", " + problem + ", " + str(nodesExplored) + ", " + str(arcsRevised)
             call("echo \"" + lineOut + "\" >> "+logFile,shell=True)
+            print(lineOut)
 
-        MACSDOutput = check_output("java -classpath " + binFolder + " -ea MACSolver " + cspFolder + file ,shell=True)
-        branchSearch = branchPatern.search(MACSDOutput)
-        nodesExplored = branchSearch.group(1)
-        arcSearch = arcPatern.search(MACSDOutput)    
-        arcsRevised = arcSearch.group(1)
-        lineOut = "Maintaining Arc Consistency, Smallest Domain, " + problem + ", " + str(nodesExplored) + ", " + str(arcsRevised)
-        call("echo \"" + lineOut + "\" >> "+logFile,shell=True)
+ #        MACSDOutput = check_output("java -classpath " + binFolder + " -ea MACSolver " + cspFolder + file ,shell=True)
+ #        branchSearch = branchPatern.search(MACSDOutput)
+ #        nodesExplored = branchSearch.group(1)
+ #        arcSearch = arcPatern.search(MACSDOutput)    
+ #        arcsRevised = arcSearch.group(1)
+ #        lineOut = "Maintaining Arc Consistency, Smallest Domain, " + problem + ", " + str(nodesExplored) + ", " + str(arcsRevised)
+ #        call("echo \"" + lineOut + "\" >> "+logFile,shell=True)
+	# print(lineOut)
 
-        for h in heuristics:
-            MACOutput = check_output("java -classpath " + binFolder + " -ea MACSolver " + cspFolder + file + " " + cspFolder+problem+h+".csph",shell=True)
-            branchSearch = branchPatern.search(MACOutput)
-            nodesExplored = branchSearch.group(1)
-            arcSearch = arcPatern.search(MACOutput)    
-            arcsRevised = arcSearch.group(1)
-            lineOut = "Maintaining Arc Consistency, " + h + ", " + problem + ", " + str(nodesExplored) + ", " + str(arcsRevised)
-            call("echo \"" + lineOut + "\" >> "+logFile,shell=True)
-
-
-
-
-
-# print(check_output("ls " + cspFolder + " | grep -e .csp$",shell=True))
-
-
-
-# call("rm -f " + logFile + " && touch " + logFile + " && echo \"instance_id,steps,optLevel,foundSolution,compTime\" >> "+logFile,shell=True)
-
-
-# for root, dirs, files in os.walk("."):
-#     for file in files:
-#         filePath = os.path.join(root,file)
-#         if not paramFilePatern.match(filePath) or re.search(r'stac', filePath):
-#             continue
-#         else:
-#             fileSearch = fileNamePatern.search(filePath)
-#                         instance_id = fileSearch.group(1)
-#                         steps  = fileSearch.group(2)
-
-#             for flag in optFlags:
-#                 lineOut = str(instance_id) + "," + str(steps) + ","
-#                 print(file,flag)
-#                 subOut = sub.check_output("TIMEFORMAT='%U'; { time ../savilerow Bombastic.eprime "+filePath+" -run-solver "+flag+"; } 2>&1",shell=True)
-#                 lineOut += flag[-1] + ","
-#                 if solutionPatern.search(subOut):
-#                     lineOut += "true,"
-#                 else:
-#                     lineOut += "false,"
-                    
-#                 match = timePatern.search(subOut)
-#                 lineOut += match.group(0)
-#                 sub.call("echo \"" + lineOut + "\" >> "+logFile,shell=True)
-
+ #        for h in heuristics:
+ #            MACOutput = check_output("java -classpath " + binFolder + " -ea MACSolver " + cspFolder + file + " " + cspFolder+problem+h+".csph",shell=True)
+ #            branchSearch = branchPatern.search(MACOutput)
+ #            nodesExplored = branchSearch.group(1)
+ #            arcSearch = arcPatern.search(MACOutput)    
+ #            arcsRevised = arcSearch.group(1)
+ #            lineOut = "Maintaining Arc Consistency, " + h + ", " + problem + ", " + str(nodesExplored) + ", " + str(arcsRevised)
+ #            call("echo \"" + lineOut + "\" >> "+logFile,shell=True)
+	#     print(lineOut)
