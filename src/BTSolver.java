d29961252c8bf55442f84dd26e4ab453c8c0c7eb
Daniel Vente <danvente@gmail.com>
2018-06-16 17:18:52 +0100

    add author tag to relevant files that I didn't write

--- src/BTSolver.java
+++ src/BTSolver.java
@@ -1,151 +1,150 @@
 /*
- * @author 170008773
- * The Class BTSolver.
+ * @author 170008773 The Class BTSolver.
  */
 public class BTSolver {
 
-	/** The problem to solve. */
-	BinaryCSP problem;
+    /** The problem to solve. */
+    BinaryCSP problem;
 
-	/** The nodes number of explored. */
-	long nodesExplored;
+    /** The nodes number of explored. */
+    long nodesExplored;
 
-	/**
-	 * Instantiates a new Backtrack solver.
-	 *
-	 * @param problem
-	 *            the problem
-	 */
-	public BTSolver(BinaryCSP problem) {
+    /**
+     * Instantiates a new Backtrack solver.
+     *
+     * @param problem
+     *            the problem
+     */
+    public BTSolver(BinaryCSP problem) {
 
-		super();
-		this.problem = problem;
-	}
+        super();
+        this.problem = problem;
+    }
 
-	/**
-	 * Prints the solution.
-	 */
-	public void printSolution() {
+    /**
+     * Prints the solution.
+     */
+    public void printSolution() {
 
-		assert problem.isConsistent();
+        assert problem.isConsistent();
 
-		StringBuffer result = new StringBuffer();
-		result.append("Branches explored: " + nodesExplored + "\n");
-		result.append("Number of Variables: " + problem.getNoVariables() + "\n");
-		result.append("Number of Constraints: " + Integer.toString(problem.getNoConstraints()) + "\n");
-		result.append("Solution: \n");
-		for (int i = 0; i < problem.getVars().size(); i++) {
-			result.append(problem.getVars().get(i).toString() + "\n");
-		}
-		System.out.println(result);
-	}
+        StringBuffer result = new StringBuffer();
+        result.append("Branches explored: " + nodesExplored + "\n");
+        result.append("Number of Variables: " + problem.getNoVariables() + "\n");
+        result.append("Number of Constraints: " + Integer.toString(problem.getNoConstraints()) + "\n");
+        result.append("Solution: \n");
+        for (int i = 0; i < problem.getVars().size(); i++) {
+            result.append(problem.getVars().get(i).toString() + "\n");
+        }
+        System.out.println(result);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
 
-		return "BTSolver \n" + problem.toString();
-	}
+        return "BTSolver \n" + problem.toString();
+    }
 
-	/**
-	 * Assign a value to a variable.
-	 *
-	 * @param var
-	 *            the variable to be assigned
-	 * @param value
-	 *            the value to assing the variable
-	 */
-	public void assign(CSPVariable var, int value) {
+    /**
+     * Assign a value to a variable.
+     *
+     * @param var
+     *            the variable to be assigned
+     * @param value
+     *            the value to assing the variable
+     */
+    public void assign(CSPVariable var, int value) {
 
-		var.assign(value);
-		nodesExplored++;
-	}
+        var.assign(value);
+        nodesExplored++;
+    }
 
-	/**
-	 * Unassign a variable.
-	 *
-	 * @param var
-	 *            the variable to be unassigned
-	 */
-	public void unassign(CSPVariable var) {
+    /**
+     * Unassign a variable.
+     *
+     * @param var
+     *            the variable to be unassigned
+     */
+    public void unassign(CSPVariable var) {
 
-		var.unassign();
-	}
+        var.unassign();
+    }
 
-	/**
-	 * The main method.
-	 *
-	 * @param args
-	 *            the arguments
-	 */
-	public static void main(String[] args) {
+    /**
+     * The main method.
+     *
+     * @param args
+     *            the arguments
+     */
+    public static void main(String[] args) {
 
-		String CSPLocation = args[0];
-		BinaryCSPReader reader = new BinaryCSPReader();
-		System.out.println(CSPLocation);
-		BTSolver solver = new BTSolver(reader.readBinaryCSP(CSPLocation));
-		solver.solveCurrentProblem();
-	}
+        String CSPLocation = args[0];
+        BinaryCSPReader reader = new BinaryCSPReader();
+        System.out.println(CSPLocation);
+        BTSolver solver = new BTSolver(reader.readBinaryCSP(CSPLocation));
+        solver.solveCurrentProblem();
+    }
 
-	/**
-	 * Solve current problem.
-	 */
-	public void solveCurrentProblem() {
+    /**
+     * Solve current problem.
+     */
+    public void solveCurrentProblem() {
 
-		if (!backtrack(0)) {
-			System.out.println("No solution possible");
-		} else {
-			printSolution();
-		}
-	}
+        if (!backtrack(0)) {
+            System.out.println("No solution possible");
+        } else {
+            printSolution();
+        }
+    }
 
-	/**
-	 * Recursive backtracking, adapted from lecture slides.
-	 *
-	 * @param depth
-	 *            the depth of the recursion
-	 * @return true, if a solution was found
-	 */
-	public boolean backtrack(int depth) {
+    /**
+     * Recursive backtracking, adapted from lecture slides.
+     *
+     * @param depth
+     *            the depth of the recursion
+     * @return true, if a solution was found
+     */
+    public boolean backtrack(int depth) {
 
-		// get variable
-		CSPVariable var = problem.getVar(depth);
+        // get variable
+        CSPVariable var = problem.getVar(depth);
 
-		// try all values in the domain
-		for (int value : var.getDomain()) {
-			assign(var, value);
-			boolean consistent = true;
+        // try all values in the domain
+        for (int value : var.getDomain()) {
+            assign(var, value);
+            boolean consistent = true;
 
-			// check if all constraints are still happy
-			for (int i = 0; i < depth; i++) {
-				consistent = problem.isArcConsistent(problem.getVar(i), var);
-				if (!consistent) {
-					break;
-				}
-			}
-			if (consistent) {
-				// are we done?
-				if (depth == problem.getNoVariables() - 1) {
-					return true;
-				} else {
-					// recurse
-					if (!backtrack(depth + 1)) {
-						unassign(var);
-					} else {
-						return true;
-					}
-				}
-			} else {
-				// backtrack, we tried everything
-				unassign(var);
-				continue;
-			}
-		}
-		return false;
-	}
+            // check if all constraints are still happy
+            for (int i = 0; i < depth; i++) {
+                consistent = problem.isArcConsistent(problem.getVar(i), var);
+                if (!consistent) {
+                    break;
+                }
+            }
+            if (consistent) {
+                // are we done?
+                if (depth == problem.getNoVariables() - 1) {
+                    return true;
+                } else {
+                    // recurse
+                    if (!backtrack(depth + 1)) {
+                        unassign(var);
+                    } else {
+                        return true;
+                    }
+                }
+            } else {
+                // backtrack, we tried everything
+                unassign(var);
+                continue;
+            }
+        }
+        return false;
+    }
 
 }
--- src/BinaryCSPReader.java
+++ src/BinaryCSPReader.java
@@ -1,3 +1,8 @@
+
+/*
+ * @author Ian Miguel ijm@st-andrews.ac.uk
+ */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,163 +20,163 @@ import java.util.Set;
  */
 public final class BinaryCSPReader {
 
-	private FileReader inFR;
-	private StreamTokenizer in;
-	private Map<String, Integer> heuristicOrdering;
+    private FileReader inFR;
+    private StreamTokenizer in;
+    private Map<String, Integer> heuristicOrdering;
 
-	/**
-	 * Main (for testing)
-	 */
-	public static void main(String[] args) {
+    /**
+     * Main (for testing)
+     */
+    public static void main(String[] args) {
 
-		if (args.length != 1) {
-			System.out.println("Usage: java BinaryCSPReader <file.csp>");
-			return;
-		}
-		BinaryCSPReader reader = new BinaryCSPReader();
-		System.out.println(reader.readBinaryCSP(args[0], args[1]));
-	}
+        if (args.length != 1) {
+            System.out.println("Usage: java BinaryCSPReader <file.csp>");
+            return;
+        }
+        BinaryCSPReader reader = new BinaryCSPReader();
+        System.out.println(reader.readBinaryCSP(args[0], args[1]));
+    }
 
-	/**
-	 * File format: <no. vars> NB vars indexed from 0 We assume that the domain of
-	 * all vars is specified in terms of bounds <lb>, <ub> (one per var) Then the
-	 * list of constraints c(<varno>, <varno>) binary tuples <domain val>, <domain
-	 * val>
-	 */
-	public BinaryCSP readBinaryCSP(String fn, String heuristicLocation) {
+    /**
+     * File format: <no. vars> NB vars indexed from 0 We assume that the domain
+     * of all vars is specified in terms of bounds <lb>, <ub> (one per var) Then
+     * the list of constraints c(<varno>, <varno>) binary tuples <domain val>,
+     * <domain val>
+     */
+    public BinaryCSP readBinaryCSP(String fn, String heuristicLocation) {
 
-		try {
-			heuristicOrdering = readHeuristic(heuristicLocation);
-			inFR = new FileReader(fn);
-			in = new StreamTokenizer(inFR);
-			in.ordinaryChar('(');
-			in.ordinaryChar(')');
-			in.nextToken(); // n
-			int n = (int) in.nval;
-			CSPVariable[] vars = new CSPVariable[n];
-			for (int i = 0; i < n; i++) {
-				in.nextToken(); // ith ub
-				int lower = (int) in.nval;
-				in.nextToken(); // ','
-				in.nextToken();
-				int upper = (int) in.nval;
-				vars[i] = new CSPVariable(Integer.toString(i), heuristicOrdering.get(Integer.toString(i)), lower,
-						upper);
-			}
-			Map<CSPVariable, Map<CSPVariable, BinaryArc>> constraints = readBinaryConstraints(vars);
-			BinaryCSP csp = new BinaryCSP(vars, constraints);
-			// TESTING:
-			// System.out.println(csp) ;
-			inFR.close();
-			return csp;
-		} catch (FileNotFoundException e) {
-			System.out.println(e);
-		} catch (IOException e) {
-			System.out.println(e);
-		}
-		return null;
-	}
+        try {
+            heuristicOrdering = readHeuristic(heuristicLocation);
+            inFR = new FileReader(fn);
+            in = new StreamTokenizer(inFR);
+            in.ordinaryChar('(');
+            in.ordinaryChar(')');
+            in.nextToken(); // n
+            int n = (int) in.nval;
+            CSPVariable[] vars = new CSPVariable[n];
+            for (int i = 0; i < n; i++) {
+                in.nextToken(); // ith ub
+                int lower = (int) in.nval;
+                in.nextToken(); // ','
+                in.nextToken();
+                int upper = (int) in.nval;
+                vars[i] = new CSPVariable(Integer.toString(i), heuristicOrdering.get(Integer.toString(i)), lower,
+                        upper);
+            }
+            Map<CSPVariable, Map<CSPVariable, BinaryArc>> constraints = readBinaryConstraints(vars);
+            BinaryCSP csp = new BinaryCSP(vars, constraints);
+            // TESTING:
+            // System.out.println(csp) ;
+            inFR.close();
+            return csp;
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
 
-	public BinaryCSP readBinaryCSP(String fn) {
+    public BinaryCSP readBinaryCSP(String fn) {
 
-		try {
-			inFR = new FileReader(fn);
-			in = new StreamTokenizer(inFR);
-			in.ordinaryChar('(');
-			in.ordinaryChar(')');
-			in.nextToken(); // n
-			int n = (int) in.nval;
-			CSPVariable[] vars = new CSPVariable[n];
-			for (int i = 0; i < n; i++) {
-				in.nextToken(); // ith ub
-				int lower = (int) in.nval;
-				in.nextToken(); // ','
-				in.nextToken();
-				int upper = (int) in.nval;
-				vars[i] = new CSPVariable(Integer.toString(i), lower, upper);
-			}
-			Map<CSPVariable, Map<CSPVariable, BinaryArc>> constraints = readBinaryConstraints(vars);
-			BinaryCSP csp = new BinaryCSP(vars, constraints);
-			// TESTING:
-			// System.out.println(csp) ;
-			inFR.close();
-			return csp;
-		} catch (FileNotFoundException e) {
-			System.out.println(e);
-		} catch (IOException e) {
-			System.out.println(e);
-		}
-		return null;
-	}
+        try {
+            inFR = new FileReader(fn);
+            in = new StreamTokenizer(inFR);
+            in.ordinaryChar('(');
+            in.ordinaryChar(')');
+            in.nextToken(); // n
+            int n = (int) in.nval;
+            CSPVariable[] vars = new CSPVariable[n];
+            for (int i = 0; i < n; i++) {
+                in.nextToken(); // ith ub
+                int lower = (int) in.nval;
+                in.nextToken(); // ','
+                in.nextToken();
+                int upper = (int) in.nval;
+                vars[i] = new CSPVariable(Integer.toString(i), lower, upper);
+            }
+            Map<CSPVariable, Map<CSPVariable, BinaryArc>> constraints = readBinaryConstraints(vars);
+            BinaryCSP csp = new BinaryCSP(vars, constraints);
+            // TESTING:
+            // System.out.println(csp) ;
+            inFR.close();
+            return csp;
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
 
-	private Map<String, Integer> readHeuristic(String heuristicLocation) throws FileNotFoundException, IOException {
+    private Map<String, Integer> readHeuristic(String heuristicLocation) throws FileNotFoundException, IOException {
 
-		Map<String, Integer> h = new HashMap<String, Integer>();
-		File heuristicFile = new File(heuristicLocation);
-		try (FileReader reader = new FileReader(heuristicFile);
-				BufferedReader bufferedReader = new BufferedReader(reader)) {
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				String name = line.substring(0, line.lastIndexOf(","));
-				int order = Integer.parseInt(line.substring(line.lastIndexOf(",") + 1, line.length()));
-				h.put(name, order);
+        Map<String, Integer> h = new HashMap<String, Integer>();
+        File heuristicFile = new File(heuristicLocation);
+        try (FileReader reader = new FileReader(heuristicFile);
+                BufferedReader bufferedReader = new BufferedReader(reader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String name = line.substring(0, line.lastIndexOf(","));
+                int order = Integer.parseInt(line.substring(line.lastIndexOf(",") + 1, line.length()));
+                h.put(name, order);
 
-			}
+            }
 
-		}
-		return h;
-	}
+        }
+        return h;
+    }
 
-	/**
-	 * @param vars
-	 */
-	private Map<CSPVariable, Map<CSPVariable, BinaryArc>> readBinaryConstraints(CSPVariable[] vars) {
+    /**
+     * @param vars
+     */
+    private Map<CSPVariable, Map<CSPVariable, BinaryArc>> readBinaryConstraints(CSPVariable[] vars) {
 
-		Map<CSPVariable, Map<CSPVariable, BinaryArc>> constraints = new HashMap<CSPVariable, Map<CSPVariable, BinaryArc>>();
+        Map<CSPVariable, Map<CSPVariable, BinaryArc>> constraints = new HashMap<CSPVariable, Map<CSPVariable, BinaryArc>>();
 
-		try {
-			in.nextToken(); // 'c' or EOF
-			while (in.ttype != StreamTokenizer.TT_EOF) {
-				// scope
-				in.nextToken(); // '('
-				in.nextToken(); // var
-				CSPVariable var1 = vars[(int) in.nval];
-				in.nextToken(); // ','
-				in.nextToken(); // var
-				CSPVariable var2 = vars[(int) in.nval];
-				in.nextToken(); // ')'
+        try {
+            in.nextToken(); // 'c' or EOF
+            while (in.ttype != StreamTokenizer.TT_EOF) {
+                // scope
+                in.nextToken(); // '('
+                in.nextToken(); // var
+                CSPVariable var1 = vars[(int) in.nval];
+                in.nextToken(); // ','
+                in.nextToken(); // var
+                CSPVariable var2 = vars[(int) in.nval];
+                in.nextToken(); // ')'
 
-				// tuples
-				Set<BinaryTuple> tuples = new HashSet<BinaryTuple>();
-				in.nextToken(); // 1st allowed val of 1st tuple
-				while (!"c".equals(in.sval) && (in.ttype != StreamTokenizer.TT_EOF)) {
-					int val1 = (int) in.nval;
-					in.nextToken(); // ','
-					in.nextToken(); // 2nd val
-					int val2 = (int) in.nval;
-					tuples.add(new BinaryTuple(val1, val2));
-					in.nextToken(); // 1stallowed val of next tuple/c/EOF
-				}
-				BinaryArc c = new BinaryArc(var1, var2, tuples);
-				if (!constraints.containsKey(var1)) {
-					constraints.put(var1, new HashMap<CSPVariable, BinaryArc>());
-				}
-				constraints.get(var1).put(var2, c);
+                // tuples
+                Set<BinaryTuple> tuples = new HashSet<BinaryTuple>();
+                in.nextToken(); // 1st allowed val of 1st tuple
+                while (!"c".equals(in.sval) && (in.ttype != StreamTokenizer.TT_EOF)) {
+                    int val1 = (int) in.nval;
+                    in.nextToken(); // ','
+                    in.nextToken(); // 2nd val
+                    int val2 = (int) in.nval;
+                    tuples.add(new BinaryTuple(val1, val2));
+                    in.nextToken(); // 1stallowed val of next tuple/c/EOF
+                }
+                BinaryArc c = new BinaryArc(var1, var2, tuples);
+                if (!constraints.containsKey(var1)) {
+                    constraints.put(var1, new HashMap<CSPVariable, BinaryArc>());
+                }
+                constraints.get(var1).put(var2, c);
 
-				c = c.reverse();
-				if (!constraints.containsKey(var2)) {
-					constraints.put(var2, new HashMap<CSPVariable, BinaryArc>());
-				}
-				constraints.get(var2).put(var1, c);
+                c = c.reverse();
+                if (!constraints.containsKey(var2)) {
+                    constraints.put(var2, new HashMap<CSPVariable, BinaryArc>());
+                }
+                constraints.get(var2).put(var1, c);
 
-			}
+            }
 
-			return constraints;
-		} catch (
+            return constraints;
+        } catch (
 
-		IOException e) {
-			System.out.println(e);
-		}
-		return null;
-	}
+        IOException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
 }
